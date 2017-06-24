35f1d7292296cc793bcc3e77939d26203b7e1d84
Ray <chenxiaolei.ray@gmail.com>
2017-06-24 21:46:51 +0800

    完成mq启时在master上注册

--- MQ/src/main/java/com/ray/stormrage/MqApplication.java
+++ MQ/src/main/java/com/ray/stormrage/MqApplication.java
@@ -1,9 +1,20 @@
 package com.ray.stormrage;
 
 import com.ray.stormrage.netty.TCPServer;
+import com.ray.stormrage.util.HttpUtil;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class MqApplication {
@@ -11,9 +22,47 @@ public class MqApplication {
 	public static void main(String[] args) throws InterruptedException {
 		ConfigurableApplicationContext context = SpringApplication.run(MqApplication.class, args);
 
-		//开启Netty服务
-		TCPServer tcpServer = context.getBean(TCPServer.class);
-		tcpServer.start();
+
+		new Thread(() -> {
+			//开启Netty服务
+			TCPServer tcpServer = context.getBean(TCPServer.class);
+			try {
+				tcpServer.start();		//会直接阻塞，需要使用多线程执行这段代码。
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+
+		new Thread(() -> {
+			//在master上注册MQ自己
+			try {
+				register(context);
+			} catch (UnsupportedEncodingException | InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
 
 	}
+
+	private static void register(ConfigurableApplicationContext context) throws UnsupportedEncodingException, InterruptedException {
+		//todo 改配置文件
+		TimeUnit.SECONDS.sleep(2);
+        ConfigurableEnvironment environment = context.getEnvironment();
+		String url = environment.getProperty("register.api");
+
+		HttpPost httpPost = new HttpPost(url);
+
+		List<NameValuePair> nameValuePairs = new ArrayList<>();
+
+
+		nameValuePairs.add(new BasicNameValuePair("host", environment.getProperty("register.host")));
+		nameValuePairs.add(new BasicNameValuePair("port", environment.getProperty("tcp.port")));
+		nameValuePairs.add(new BasicNameValuePair("name", environment.getProperty("register.name")));
+
+		httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+		HttpUtil.sendHttpPost(httpPost);
+	}
+
+
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # StormrageMQ
-Master主节点 ：服务端口号3000，Netty端口号4000
+Master主节点 ：服务端口号3000
 
 MQ节点：服务端口号3001，Netty端口号4001
 
@@ -8,3 +8,9 @@ Web服务节点：服务端口号8080
 redis端口号6379
 
 mysql端口号3306
+
+## 坑点
+
+Netty启动服务后会阻塞进程，所以无法执行到.start()方法后的register()代码。
+
+@EventListener(ContextRefreshedEvent.class)这句话在spring启动的时候会调用，但是启用netty时并不好用。我最终的解决办法是开启两个线程，一个调用.start()方法启动netty服务，另一个调用register()方法在master上注册MQ。
